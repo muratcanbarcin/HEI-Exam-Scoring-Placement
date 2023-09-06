e3ef3c37d9e9cd7371d6ff84b8d4024590beb305
Murat Can Barçın <muratcanbarcin@gmail.com>
2023-09-06 16:11:01 +0300

    Proje dosyası ekle.

---
+++ HEI Exam-Scoring-Placement/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HEI_Exam_Scoring_Placement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
